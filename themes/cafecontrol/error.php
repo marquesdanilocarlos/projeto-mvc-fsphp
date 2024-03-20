eddf7a58fbf9d7b638c4f07df4b06935d0718dbb
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-03-19 22:39:08 -0300

    Aula 03 - Renderização da página 404

--- themes/cafecontrol/error.php
+++ themes/cafecontrol/error.php
@@ -1,4 +1,5 @@
-<!--404-->
+<?php
+$this->layout('theme'); ?>
 <article class="not_found">
     <div class="container content">
         <header class="not_found_header">
@@ -6,11 +7,9 @@
             <h1>Ooops. Essa página não existe :/</h1>
             <p>Sentimos muito, mas o conteúdo que você tentou acessar não existe, está indisponível ou foi removido
                 :/</p>
-            <a class="not_found_btn gradient gradient-green gradient-hover transition radius" title="home" href="./">Continue
+            <a class="not_found_btn gradient gradient-green gradient-hover transition radius" title="home"
+               href="<?= url() ?>">Continue
                 navegando</a>
         </header>
     </div>
 </article>
-
-<!--OPT OUT-->
-<?php require __DIR__ . "/optout.php"; ?>
