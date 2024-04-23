ddb42e2ae401dcb3ada8e7d97d68013c78fb7a25
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-04-22 22:47:17 -0300

    Aula 15 - Models "Post" e "PostStatus"

---
+++ source/Models/Post.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Source\Models;
+
+use Source\Core\Model;
+
+class Post extends Model
+{
+    public function __construct()
+    {
+        parent::__construct('posts', ['id'], ['title', 'id', 'subtitle', 'content', 'post_status_id']);
+    }
+
+    public function find(?string $terms = null, ?string $params = null, string $columns = '*'): Model
+    {
+        $defatultTerms = 'post_status_id = :post_status_id AND post_at <= NOW()';
+        $defaultParams = "post_status_id=" . PostStatus::POSTED;
+
+        if ($terms) {
+            $defatultTerms .= "AND {$terms}";
+        }
+
+        if ($params) {
+            $defaultParams .= "&{$params}";
+        }
+
+
+        return parent::find($defatultTerms, $defaultParams, $columns);
+    }
+
+    public function getAuthor(): ?Model
+    {
+        if (!$this->author) {
+            return null;
+        }
+
+        return (new User())->findById($this->author);
+    }
+
+    public function getCategory(): ?Model
+    {
+        if (!$this->category) {
+            return null;
+        }
+
+        return (new Category())->findById($this->author);
+    }
+
+    public function findByUri(string $uri, string $columns = '*'): ?self
+    {
+        $find = $this->find('uri = :uri', "uri={$uri}", $columns);
+        return $find->fetch();
+    }
+
+    public function save(): bool
+    {
+        /**
+         * Post Update
+         */
+        if (!empty($this->id)) {
+            $postId = $this->id;
+            $this->update($this->safe(), 'id = :id', "id={$postId}");
+
+            if ($this->getFail()) {
+                $this->message->error("Erro ao atualizar o post");
+                return false;
+            }
+
+            /**
+             * Post Create
+             */
+
+            $this->data = $this->findById($postId)->getData();
+            return true;
+        }
+    }
+}
---
+++ source/Models/PostStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Source\Models;
+
+class PostStatus
+{
+    public const int POSTED = 1;
+    public const int DRAFT = 2;
+    public const int TRASH = 3;
+}
