796518bd6fe5bb917667879738d3b1dc659cd4ed
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-05-29 23:11:27 -0300

    Aula 31 - Inclusão de usuário online na base

--- source/Controllers/Web.php
+++ source/Controllers/Web.php
@@ -8,6 +8,7 @@ use Source\Models\Category;
 use Source\Models\Faq\Question;
 use Source\Models\Post;
 use Source\Models\Report\Access;
+use Source\Models\Report\Online;
 use Source\Models\User;
 use Source\Support\Pager;
 use stdClass;
@@ -18,6 +19,7 @@ class Web extends Controller
     {
         parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_THEME . "/");
         (new Access())->report();
+        (new Online())->report();
     }
 
     public function home(): void
--- source/Models/Report/Online.php
+++ source/Models/Report/Online.php
@@ -9,19 +9,37 @@ class Online extends Model
 {
     public function __construct(
         private int $sessionTime = 20
-    )
-    {
+    ) {
         parent::__construct('report_online', ['id'], ['ip', 'url', 'agent']);
     }
 
     public function findByActive(bool $count = false)
     {
+        $online = $this->find("updated_at >= NOW() - INTERVAL {$this->sessionTime} MINUTE");
 
+        if ($count) {
+            return $online->count();
+        }
+
+        return $online->fetch(true);
     }
 
     public function report(): self
     {
+        $session = new Session();
 
+        if (!$session->has('online')) {
+            $this->user = $session->authUser ?? null;
+            $this->url = filter_input(INPUT_GET, 'route', FILTER_SANITIZE_SPECIAL_CHARS) ?? '/';
+            $this->ip = filter_input(INPUT_SERVER, 'REMOTE_ADDR');
+            $this->agent = filter_input(INPUT_SERVER, 'HTTP_USER_AGENT');
+
+            $this->save();
+            $session->set('online', $this->id);
+            return $this;
+        }
+
+        return $this;
     }
 
     public function save(): bool
